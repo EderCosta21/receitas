b4d8c3f01b516ab1f84c1c61e8351eca3f0584c6
Eder Costa <69824696+EderCosta21@users.noreply.github.com>
2023-04-05 13:51:16 -0300

    tabBar

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {Ionicons} from '@expo/vector-icons';
 
 import  {Home}  from "../pages/home";
 import  {Favorites}  from "../pages/favorites";
@@ -7,9 +8,42 @@ const Tab = createBottomTabNavigator();
 
 export function Routes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home}  />
-      <Tab.Screen name="Favorites" component={Favorites} />
+    <Tab.Navigator
+    screenOptions={{
+        headerShown:false,
+        tabBarHideOnKeyboard:true,
+        tabBarShowLabel:false,
+        tabBarActiveTintColor:'purple',
+        tabBarStyle:{
+            backgroundColor:'#FFF',
+            borderBottomWidth:0,
+        }
+    }}
+    >
+      <Tab.Screen 
+      name="Home" 
+      component={Home}  
+      options={{
+        tabBarIcon:({color,size,focused})=>{
+            if(focused){
+               return <Ionicons name="home" color="#000" size={size}></Ionicons>
+            }
+            return <Ionicons name="home-outline" color={color} size={18}></Ionicons>
+        }
+      }}
+      />
+      <Tab.Screen 
+      name="Favorites" 
+      component={Favorites} 
+      options={{
+        tabBarIcon:({color,size,focused})=>{
+            if(focused){
+               return <Ionicons name="heart" color="#FF4141" size={size}></Ionicons>
+            }
+            return <Ionicons name="heart-outline" color={color} size={size}></Ionicons>
+        }
+      }}
+      />
     </Tab.Navigator>
   );
 }
